49ad3507c80f790f2e76d4b9597b9bec529be517
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-10-18 21:52:17 -0300

    Desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,37 @@
+const actFavs = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $in: actFavs },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: { $setIntersection: ["$cast", actFavs] },
+      },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+]);
