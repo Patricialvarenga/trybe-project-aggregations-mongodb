2cf8696a4780cd16a308d78f840759bbbf3a6142
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-10-20 10:30:42 -0300

    Desafio 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,29 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      diaDaSemana: {
+        $dayOfWeek:
+          "$startTime",
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$diaDaSemana",
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  { $limit: 1 },
+]);
